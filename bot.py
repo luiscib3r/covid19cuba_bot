aee81e3cabe97b93c2f756a99f65640f26c7e565
Luis Correa <correaleyval@gmail.com>
2020-04-01 19:00:35 -0400

    More info

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ import mdb
 bot = telebot.TeleBot(config.token)
 
 def summary():
-    message = "🤒 Diagnosticados: {}\n🤧 Activos: {}\n😃 Recuperados: {}\n🤩 Índice de Recuperación: {}%\n✈️ Evacuados: {}\n⚰️ Fallecidos: {}\n😵 Mortalidad: {}%\n🏥 Ingresados {}\n📆 Actualizado: {}"
+    message = "🤒 Diagnosticados: {}\n🤧 Activos: {}\n😃 Recuperados: {}\n🤩 Índice de Recuperación: {}%\n✈️ Evacuados: {}\n⚰️ Fallecidos: {}\n😵 Mortalidad: {}%\n🏥 Ingresados {}\n📆 Actualizado: {}\n\n Más Información en @covid19cubadata_bot"
 
     data = requests.get(config.api_url + '/summary').json()
 

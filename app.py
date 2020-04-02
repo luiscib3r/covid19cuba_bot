d565ea2edb78ac48782958db76f92def35789170
Luis Correa <correaleyval@gmail.com>
2020-04-01 21:39:59 -0400

    RMESSAGE

--- app.py
+++ app.py
@@ -34,12 +34,31 @@ def send_alert():
         except ApiException:
             mdb.removechat(chat)
 
+def send_remember():
+    message = '''Mantenerse informado de las medidas adoptadas por las autoridades de salud de su país forma parte de tu obligación como ciudadano en la lucha contra la pandemia de la COVID-19. En unos minutos, justo a las 11 de la mañana, el Ministerio de Salud Pública de la República de Cuba comenzará su habitual Conferencia de Prensa. No dejes de verla, mantenernos informados es vital para apoyar a las autoridades a erradicar este mal que nos azota a todos.'''
+
+    chats = mdb.allchats()
+
+    for chat in chats:
+        try:
+            bot.send_message(chat, message)
+        except ApiException:
+            mdb.removechat(chat)
+
+@server.route('/remember')
+def remember():
+    Pool().apply_async(send_alert)
+
+    return jsonify({
+        'message': 'remember',
+    })
+
 @server.route('/alert')
 def alert():
     Pool().apply_async(send_alert)
 
     return jsonify({
-        'message': alert,
+        'message': 'alert',
     })
 
 @server.route('/notify')

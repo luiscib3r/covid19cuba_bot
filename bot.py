eb7b9a1a7ec45fe99a3f30343e6f325e44a87226
Luis Correa <correaleyval@gmail.com>
2020-04-02 18:34:02 -0400

    fix

--- bot.py
+++ bot.py
@@ -47,7 +47,21 @@ https://github.com/correaleyval/covid19cuba_bot
 https://github.com/correaleyval/covid19cuba_api
 https://github.com/correaleyval/covid19cuba_async'''
 
-#hide_keyboard = types.ReplyKeyboardRemove()
+markup = types.ReplyKeyboardMarkup(row_width=1)
+    
+markup.add(
+    types.KeyboardButton('☢️ Resumen'),
+    types.KeyboardButton('☣️ Resumen con Gráficos'),
+    types.KeyboardButton('⏳ Evolución de casos por días'),
+    types.KeyboardButton('📝 Datos de los Tests realizados'),
+    types.KeyboardButton('🇨🇺 Casos por provincias'),
+    types.KeyboardButton('🚻 Casos por Sexo'),
+    types.KeyboardButton('👶🏻🧔🏽 Distribución por grupos etarios'),
+    types.KeyboardButton('🦠 Modo de Contagio'),
+    types.KeyboardButton('🌏 Casos por Nacionalidad (Cubanos/Extranjeros)'),
+    types.KeyboardButton('🗺 Distribución por nacionalidad'),
+    types.KeyboardButton('ℹ️ Acerca de')
+)
 
 @bot.message_handler(commands=['start'])
 def start_summary(message):
@@ -56,22 +70,6 @@ def start_summary(message):
 
     bot.send_chat_action(cid, 'typing')
     mdb.savechat(cid)
-
-    markup = types.ReplyKeyboardMarkup(row_width=1)
-    
-    markup.add(
-        types.KeyboardButton('☢️ Resumen'),
-        types.KeyboardButton('☣️ Resumen con Gráficos'),
-        types.KeyboardButton('⏳ Evolución de casos por días'),
-        types.KeyboardButton('📝 Datos de los Tests realizados'),
-        types.KeyboardButton('🇨🇺 Casos por provincias'),
-        types.KeyboardButton('🚻 Casos por Sexo'),
-        types.KeyboardButton('👶🏻🧔🏽 Distribución por grupos etarios'),
-        types.KeyboardButton('🦠 Modo de Contagio'),
-        types.KeyboardButton('🌏 Casos por Nacionalidad (Cubanos/Extranjeros)'),
-        types.KeyboardButton('🗺 Distribución por nacionalidad'),
-        types.KeyboardButton('ℹ️ Acerca de')
-    )
 
     bot.send_message(
         cid,
@@ -88,7 +86,8 @@ def about_handler(message):
 
     bot.send_message(
         cid,
-        about()
+        about(),
+        reply_markup=markup
     )
 
 @bot.message_handler(commands=['summary'])
@@ -100,7 +99,8 @@ def send_summary(message):
 
     bot.send_message(
         cid,
-        summary()
+        summary(),
+        reply_markup=markup
     )
 
     graph1 = requests.get(config.api_url + '/summary_graph1').content
@@ -131,8 +131,8 @@ def send_evolution(message):
     with open('evolution_fallecidos.png', 'wb') as f:
         f.write(fallecidos_graph)
 
-    bot.send_photo(cid, open('evolution.png', 'rb')
-    bot.send_photo(cid, open('evolution_fallecidos.png', 'rb')
+    bot.send_photo(cid, open('evolution.png', 'rb'), reply_markup=markup)
+    bot.send_photo(cid, open('evolution_fallecidos.png', 'rb'), reply_markup=markup)
 
 
 @bot.message_handler(commands=['sexo'])
@@ -150,7 +150,7 @@ def send_sexo(message):
 
     texto = 'Hombres: {} | Mujeres {}'.format(data['hombres'], data['mujeres'])
 
-    bot.send_photo(cid, open('sexo.png', 'rb'), tex)
+    bot.send_photo(cid, open('sexo.png', 'rb'), texto, reply_markup=markup)
 
 @bot.message_handler(commands=['modo'])
 def send_modo(message):
@@ -164,7 +164,7 @@ def send_modo(message):
     with open('modo.png', 'wb') as f:
         f.write(modo_graph)
 
-    bot.send_photo(cid, open('modo.png', 'rb)
+    bot.send_photo(cid, open('modo.png', 'rb'), reply_markup=markup)
 
 @bot.message_handler(commands=['casos_extranjeros'])
 def send_casos_extranjeros(message):
@@ -178,7 +178,7 @@ def send_casos_extranjeros(message):
     with open('casos_extranjeros.png', 'wb') as f:
         f.write(casos_extranjeros_graph)
 
-    bot.send_photo(cid, open('casos_extranjeros.png', 'rb)
+    bot.send_photo(cid, open('casos_extranjeros.png', 'rb'), reply_markup=markup)
 
 @bot.message_handler(commands=['nacionalidad'])
 def send_nacionalidad(message):
@@ -195,7 +195,7 @@ def send_nacionalidad(message):
 
     texto = 'Cubanos: {} | Extranjeros {}'.format(data['Cubanos'], data['Extranjeros'])
 
-    bot.send_photo(cid, open('nacionalidad.png', 'rb'), tex)
+    bot.send_photo(cid, open('nacionalidad.png', 'rb'), texto, reply_markup=markup)
 
 @bot.message_handler(commands=['edad'])
 def send_edad(message):
@@ -209,7 +209,7 @@ def send_edad(message):
     with open('edad.png', 'wb') as f:
         f.write(edad_graph)
 
-    bot.send_photo(cid, open('edad.png', 'rb)
+    bot.send_photo(cid, open('edad.png', 'rb'), reply_markup=markup)
 
 @bot.message_handler(commands=['test'])
 def send_test(message):
@@ -223,7 +223,7 @@ def send_test(message):
     with open('test.png', 'wb') as f:
         f.write(test_graph)
 
-    bot.send_photo(cid, open('test.png', 'rb)
+    bot.send_photo(cid, open('test.png', 'rb'), reply_markup=markup)
 
 @bot.message_handler(commands=['provincias'])
 def send_provincias(message):
@@ -241,8 +241,8 @@ def send_provincias(message):
     with open('municipios.png', 'wb') as f:
         f.write(municipios_graph)
 
-    bot.send_photo(cid, open('provincias.png', 'rb)
-    bot.send_photo(cid, open('municipios.png', 'rb)
+    bot.send_photo(cid, open('provincias.png', 'rb'), reply_markup=markup)
+    bot.send_photo(cid, open('municipios.png', 'rb'), reply_markup=markup)
 
 @bot.message_handler(commands=['notify'])
 def notify(message):

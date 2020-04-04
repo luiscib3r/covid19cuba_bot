5433a4a3ef605657d33337473a5494d862be1ff7
Luis Correa <correaleyval@gmail.com>
2020-04-04 16:25:53 -0400

    Divide && Konquer
    Espero que funcione :smile:

--- app.py
+++ app.py
@@ -27,43 +27,36 @@ def send_notify():
 
 import rmessages
 
-def send_alert():
-    chats = mdb.allchats()
+def send_alert(arr):
 
-    for chat in chats:
+    for chat in arr:
         try:
             bot.send_message(chat, rmessages.getMessage())
         except ApiException:
             mdb.removechat(chat)
 
-def send_remember():
+def send_remember(arr):
     message = '''Mantenerse informado de las medidas adoptadas por las autoridades de salud de su país forma parte de tu obligación como ciudadano en la lucha contra la pandemia de la COVID-19. En unos minutos, justo a las 11 a.m, el Ministerio de Salud Pública de la República de Cuba comenzará su habitual Conferencia de Prensa. No dejes de verla, mantenernos informados es vital para apoyar a las autoridades a erradicar este mal que nos azota a todos.'''
 
-    chats = mdb.allchats()
-
-    for chat in chats:
+    for chat in arr:
         try:
             bot.send_message(chat, message)
         except ApiException:
             mdb.removechat(chat)
 
-def send_claps():
+def send_claps(arr):
     message = '''Los nuevos super héroes merecen ser reverenciados. Gracias al  personal de salud por servir de ejército activo contra esta pandemia. Salgamos todos a dar un aplauso fuerte como merecen justo a las 9 p.m'''
 
-    chats = mdb.allchats()
-
-    for chat in chats:
+    for chat in arr:
         try:
             bot.send_message(chat, message)
         except ApiException:
             mdb.removechat(chat)
 
-def send_updating():
+def send_updating(arr):
     message = '''ℹ️ Se está trabajando en la actualización de los datos se enviará una notificación cuando se encuentre lista la información'''
 
-    chats = mdb.allchats()
-
-    for chat in chats:
+    for chat in arr:
         try:
             bot.send_message(chat, message)
         except ApiException:
@@ -78,7 +71,12 @@ def updating():
             'message': 'alert'
         })
 
-    Pool().apply_async(send_updating)
+    chats = mdb.allchats()
+    
+    for s,e in enumerate(range(500, len(chats)+500, 500)):
+        Pool().apply_async(send_updating, args=(chats[s*500:e],))
+
+    Pool().apply_async(send_updating, args=(chats[e:],))
 
     return jsonify({
         'message': 'updating',
@@ -93,8 +91,12 @@ def claps():
             'message': 'claps'
         })
 
+    chats = mdb.allchats()
+    
+    for s,e in enumerate(range(500, len(chats)+500, 500)):
+        Pool().apply_async(send_claps, args=(chats[s*500:e],))
 
-    Pool().apply_async(send_claps)
+    Pool().apply_async(send_claps, args=(chats[e:],))
 
     return jsonify({
         'message': 'claps',
@@ -109,7 +111,12 @@ def remember():
             'message': 'remember'
         })
 
-    Pool().apply_async(send_remember)
+    chats = mdb.allchats()
+    
+    for s,e in enumerate(range(500, len(chats)+500, 500)):
+        Pool().apply_async(send_remember, args=(chats[s*500:e],))
+
+    Pool().apply_async(send_remember, args=(chats[e:],))
 
     return jsonify({
         'message': 'remember',
@@ -124,7 +131,12 @@ def alert():
             'message': 'alert'
         })
 
-    Pool().apply_async(send_alert)
+    chats = mdb.allchats()
+    
+    for s,e in enumerate(range(500, len(chats)+500, 500)):
+        Pool().apply_async(send_alert, args=(chats[s*500:e],))
+
+    Pool().apply_async(send_alert, args=(chats[e:],))
 
     return jsonify({
         'message': 'alert',
@@ -139,7 +151,12 @@ def notify():
             'message': 'notify'
         })
 
-    Pool().apply_async(send_notify)    
+    chats = mdb.allchats()
+    
+    for s,e in enumerate(range(500, len(chats)+500, 500)):
+        Pool().apply_async(send_notify, args=(chats[s*500:e],))
+
+    Pool().apply_async(send_notify, args=(chats[e:],))
 
     return jsonify({
         'message': 'notify'
